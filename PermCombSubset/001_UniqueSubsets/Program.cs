300c06248a17d6cc24ab95cb29abf00d1a965f36
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-08-03 15:56:44 +0530

    Given array of unique to subsets

---
+++ PermCombSubset/001_UniqueSubsets/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_UniqueSubsets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 1, 2, 3};
+            var ans = GetSubSets(nums);
+            for (int i = 0; i < ans.Count; i++)
+            {
+                for (int j = 0; j < ans[i].Count; j++)
+                {
+                    Console.Write(ans[i][j]+", ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static IList<IList<int>> GetSubSets(int[] arr)
+        {
+            var ans = new List<IList<int>>();
+            ans.Add(new List<int>());
+            foreach (var item in arr)
+            {
+                var newAns = new List<IList<int>>();
+                foreach (var nums in ans)
+                {
+                    var cpy = nums.ToList();
+                    cpy.Add(item);
+                    newAns.Add(cpy);
+                }
+                ans.AddRange(newAns);
+            }
+
+            return ans;
+        }
+    }
+}
